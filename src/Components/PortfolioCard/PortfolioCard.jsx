c900ff95e321dc3e8ffc0125e104aebaf92622f0
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-02-06 14:18:30 -0300

    Add props, and links to PortfolioCard component

--- src/Components/PortfolioCard/PortfolioCard.jsx
+++ src/Components/PortfolioCard/PortfolioCard.jsx
@@ -1,10 +1,29 @@
+import { NavLink } from 'react-router-dom';
 import './PortfolioCard.css';
 
-const PortfolioCard = () => {
+const PortfolioCard = ({
+	image,
+	title,
+	description,
+	repositoryUrl,
+	websiteUrl,
+}) => {
 	return (
 		<div className='card-container'>
 			<div className='card-content'>
-				<img src='' alt='' />
+				<img src={image} alt='Two children playing' />
+				<h2 className='project-title'>{title}</h2>
+				<div className='project-detail'>
+					<p>{description}</p>
+					<div className='project-links'>
+						<NavLink className='repository' to={repositoryUrl} target='_blank'>
+							<b>Repository</b>
+						</NavLink>
+						<NavLink className='website' to={websiteUrl} target='_blank'>
+							<b>Website</b>
+						</NavLink>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
