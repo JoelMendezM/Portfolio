a8c16a64bedf24a76829e26f6bd8b5c1ddd09760
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-02-17 12:03:16 -0300

    Add Who I am info - code and style

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,5 +1,16 @@
 import { Image } from '@chakra-ui/image';
-import { Center, Grid, GridItem, Link, Text } from '@chakra-ui/react';
+import {
+	Box,
+	Center,
+	Grid,
+	GridItem,
+	Heading,
+	Link,
+	ListItem,
+	Text,
+	UnorderedList,
+} from '@chakra-ui/react';
+import { FaRegLaughBeam } from 'react-icons/fa';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/tabs';
 import bootstrap from '../../assets/skills/bootstrap.svg.png';
 import css from '../../assets/skills/css.png';
@@ -24,236 +35,311 @@ import alkemy from '../../assets/background/alkemy.png';
 import coderhouse from '../../assets/background/coderhouse.png';
 import degree from '../../assets/background/bachelor-degree.jpg';
 import './AboutMe.css';
+import Carousel from '../Carousel/Carousel';
+import salsa from '../../assets/salsa-casino.mp4';
 
 const AboutMe = () => {
 	return (
-		<Tabs defaultIndex={0} isFitted variant='enclosed' bg='black' pt='5rem'>
-			<TabList>
-				<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
-					Skill Set
-				</Tab>
-				<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
-					Background
-				</Tab>
-				<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
-					Who I am
-				</Tab>
-				<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
-					Hobbies
-				</Tab>
-			</TabList>
-			<TabPanels>
-				<TabPanel>
-					<Grid
-						templateRows={[
-							'repeat(8, 1fr)',
-							'repeat(4, 1fr)',
-							'repeat(3, 1fr)',
-						]}
-						templateColumns={[
-							'repeat(2, 1fr)',
-							'repeat(3, 1fr)',
-							'repeat(4, 1fr)',
-						]}
-						gap={2}
-						fontWeight={500}
-					>
-						<GridItem>
-							<Center flexDirection='column' className='test'>
-								<Image w={120} h={130} src={bootstrap} />
-								<Text>Bootstrap</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={firebase} />
-								<Text>Firebase</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={css} />
-								<Text>CSS3</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={git} />
-								<Text>Git</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={github} />
-								<Text>Github</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={html} />
-								<Text>HTML5</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={jira} />
-								<Text>Jira</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={js} />
-								<Text>JavaScript</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={react} />
-								<Text>React JS</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={redux} />
-								<Text>Redux</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={ruby} />
-								<Text>Ruby</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={chakra} />
-								<Text>Chakra UI</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={sass} />
-								<Text>Sass</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={shopify} />
-								<Text>Shopify</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={slack} />
-								<Text>Slack</Text>
-							</Center>
-						</GridItem>
-						<GridItem>
-							<Center flexDirection='column'>
-								<Image w={120} h={130} src={trello} />
-								<Text>Trello</Text>
-							</Center>
-						</GridItem>
-					</Grid>
-				</TabPanel>
-				<TabPanel>
-					<Grid
-						templateRows={[
-							'repeat(1, 1fr)',
-							'repeat(1, 1fr)',
-							'repeat(1, 1fr)',
-						]}
-						templateColumns={[
-							'repeat(3, 1fr)',
-							'repeat(3, 1fr)',
-							'repeat(3, 1fr)',
-						]}
-						gap={1}
-						fontWeight={700}
-					>
-						<GridItem
-							h={660}
-							display='flex'
-							alignItems='center'
-							justifyContent='center'
-							className='container-upwork'
+		<>
+			<Heading
+				fontSize='3rem'
+				fontWeight='800'
+				color='white'
+				backgroundColor='black'
+				pt='4rem'
+				textAlign='center'
+			>
+				ABOUT ME
+			</Heading>
+			<Tabs defaultIndex={0} isFitted variant='enclosed' bg='black' pt='2rem'>
+				<TabList>
+					<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
+						Skill Set
+					</Tab>
+					<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
+						Background
+					</Tab>
+					<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
+						Who I am
+					</Tab>
+					<Tab p={['0.3rem', '1rem', '1.5rem']} color='white'>
+						Hobbies
+					</Tab>
+				</TabList>
+				<TabPanels>
+					<TabPanel>
+						<Grid
+							templateRows={[
+								'repeat(8, 1fr)',
+								'repeat(4, 1fr)',
+								'repeat(3, 1fr)',
+							]}
+							templateColumns={[
+								'repeat(2, 1fr)',
+								'repeat(3, 1fr)',
+								'repeat(4, 1fr)',
+							]}
+							gap={2}
+							fontWeight={500}
 						>
-							<Center flexDirection='column'>
-								<Text className='title-upwork'>FREELANCE UPWORK PLATFORM</Text>
-								<Center className='background-upwork'>
-									<Image src={top} />
-									<Image src={reviews} />
-									<Image src={marriott} />
-									<Text>If you want to take a look:</Text>
-									<Link
-										href='https://www.upwork.com/freelancers/~010243cd1d7e26063d?viewMode=1'
-										isExternal
-									>
-										Click here!
-									</Link>
+							<GridItem>
+								<Center flexDirection='column' className='test'>
+									<Image w={120} h={130} src={bootstrap} />
+									<Text>Bootstrap</Text>
 								</Center>
-							</Center>
-						</GridItem>
-						<GridItem
-							borderRight='1px solid white;'
-							borderLeft='1px solid white;'
-							h={600}
-							display='flex'
-							alignItems='center'
-							justifyContent='center'
-							className='container-front-end'
-						>
-							<Center>
-								<Text className='title-front-end'>FRONT-END</Text>
-								<Center className='background-front-end'>
-									<Image src={alkemy} className='alkemy' />
-									<Text>Acceleration: </Text>
-									<Link
-										className='alkemy-link'
-										href='https://assets.alkemy.org/certificates/google-oauth2%7C106402354811067069167/0688aca1-fe51-4ba8-9995-700af7f5936f.pdf'
-										isExternal
-									>
-										Certificate
-									</Link>
-									<Image src={coderhouse} className='coder' />
-									<Text>Front-end Carrier:</Text>
-									<Link>React Js</Link>
-									<Link>JavaScript</Link>
-									<Link>Desarrollo Web</Link>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={firebase} />
+									<Text>Firebase</Text>
 								</Center>
-							</Center>
-						</GridItem>
-						<GridItem
-							h={600}
-							display='flex'
-							alignItems='center'
-							justifyContent='center'
-							textAlign='center'
-							className='container-academic'
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={css} />
+									<Text>CSS3</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={git} />
+									<Text>Git</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={github} />
+									<Text>Github</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={html} />
+									<Text>HTML5</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={jira} />
+									<Text>Jira</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={js} />
+									<Text>JavaScript</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={react} />
+									<Text>React JS</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={redux} />
+									<Text>Redux</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={ruby} />
+									<Text>Ruby</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={chakra} />
+									<Text>Chakra UI</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={sass} />
+									<Text>Sass</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={shopify} />
+									<Text>Shopify</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={slack} />
+									<Text>Slack</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Image w={120} h={130} src={trello} />
+									<Text>Trello</Text>
+								</Center>
+							</GridItem>
+						</Grid>
+					</TabPanel>
+					<TabPanel>
+						<Grid
+							templateRows={[
+								'repeat(1, 1fr)',
+								'repeat(1, 1fr)',
+								'repeat(1, 1fr)',
+							]}
+							templateColumns={[
+								'repeat(3, 1fr)',
+								'repeat(3, 1fr)',
+								'repeat(3, 1fr)',
+							]}
+							gap={1}
+							fontWeight={700}
 						>
-							<Center>
-								<Text className='title-academic'>ACADEMIC - UNIVERSITY</Text>
-								<Center className='background-academic'>
-									<Text>
-										I got a <strong>{`CHEMICAL BACHELOR'S DEGREE`}</strong>{' '}
-										{`'2009 -
-										2015'`}
+							<GridItem
+								h={660}
+								display='flex'
+								alignItems='center'
+								justifyContent='center'
+								className='container-upwork'
+							>
+								<Center flexDirection='column'>
+									<Text className='title-upwork'>
+										FREELANCE UPWORK PLATFORM
 									</Text>
-									<Image src={degree} />
+									<Center className='background-upwork'>
+										<Image src={top} />
+										<Image src={reviews} />
+										<Image src={marriott} />
+										<Text>If you want to take a look:</Text>
+										<Link
+											href='https://www.upwork.com/freelancers/~010243cd1d7e26063d?viewMode=1'
+											isExternal
+										>
+											Click here!
+										</Link>
+									</Center>
 								</Center>
-							</Center>
-						</GridItem>
-					</Grid>
-				</TabPanel>
-				<TabPanel>
-					<Image
-						boxSize='200px'
-						fit='cover'
-						src='https://www.freepnglogos.com/uploads/naruto-png/image-kunai-naruto-fictional-battle-omniverse-23.png'
-					/>
-				</TabPanel>
-			</TabPanels>
-		</Tabs>
+							</GridItem>
+							<GridItem
+								borderRight='1px solid white;'
+								borderLeft='1px solid white;'
+								h={600}
+								display='flex'
+								alignItems='center'
+								justifyContent='center'
+								className='container-front-end'
+							>
+								<Center>
+									<Text className='title-front-end'>FRONT-END</Text>
+									<Center className='background-front-end'>
+										<Image src={alkemy} className='alkemy' />
+										<Text>Acceleration: </Text>
+										<Link
+											className='alkemy-link'
+											href='https://assets.alkemy.org/certificates/google-oauth2%7C106402354811067069167/0688aca1-fe51-4ba8-9995-700af7f5936f.pdf'
+											isExternal
+										>
+											Certificate
+										</Link>
+										<Image src={coderhouse} className='coder' />
+										<Text>Front-end Carrier:</Text>
+										<Link>React Js</Link>
+										<Link>JavaScript</Link>
+										<Link>Desarrollo Web</Link>
+									</Center>
+								</Center>
+							</GridItem>
+							<GridItem
+								h={600}
+								display='flex'
+								alignItems='center'
+								justifyContent='center'
+								textAlign='center'
+								className='container-academic'
+							>
+								<Center>
+									<Text className='title-academic'>ACADEMIC - UNIVERSITY</Text>
+									<Center className='background-academic'>
+										<Text>
+											I got a <strong>{`CHEMICAL BACHELOR'S DEGREE`}</strong>{' '}
+											{`'2009 -
+										2015'`}
+										</Text>
+										<Image src={degree} />
+									</Center>
+								</Center>
+							</GridItem>
+						</Grid>
+					</TabPanel>
+					<TabPanel>
+						<Center flexDirection='column'>
+							<Text fontSize={['lg', 'xl', '3xl']} textAlign='center'>
+								I consider myself an enthusiastic, intelligent and curious
+								person. Always looking to learn new things and grow every day as
+								a professional and as a person.
+							</Text>
+							<Box className='container-soft-skills'>
+								<Text fontSize={['md', 'lg', 'xl']}>SOFT SKILLS:</Text>
+								<UnorderedList fontSize={['sm', 'md', 'lg']} color='white'>
+									<ListItem>Problem resolution</ListItem>
+									<ListItem>Teamwork</ListItem>
+									<ListItem>Effective communication</ListItem>
+									<ListItem>Time management</ListItem>
+									<ListItem>Critical thinking</ListItem>
+									<ListItem>Adaptability</ListItem>
+									<ListItem>Continuous learning</ListItem>
+									<ListItem>Creativity</ListItem>
+								</UnorderedList>
+							</Box>
+						</Center>
+					</TabPanel>
+					<TabPanel>
+						<Grid
+							templateRows={[
+								'repeat(4, 1fr)',
+								'repeat(2, 1fr)',
+								'repeat(1, 1fr)',
+							]}
+							templateColumns={[
+								'repeat(1, 1fr)',
+								'repeat(2, 1fr)',
+								'repeat(4, 1fr)',
+							]}
+							gap={2}
+						>
+							<GridItem>
+								<Center flexDirection='column'>
+									<Carousel />
+									<Text>Travel Lover</Text>
+								</Center>
+							</GridItem>
+							<GridItem className='dance'>
+								<Center flexDirection='column'>
+									<video controls>
+										<source src={salsa} type='video/mp4' />
+									</video>
+									<Text flexDirection='row'>
+										Dance Lover (me: red shoes {<FaRegLaughBeam />})
+									</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center>
+									<Image />
+									<Text>description</Text>
+								</Center>
+							</GridItem>
+							<GridItem>
+								<Center>
+									<Image />
+									<Text>description</Text>
+								</Center>
+							</GridItem>
+						</Grid>
+					</TabPanel>
+				</TabPanels>
+			</Tabs>
+		</>
 	);
 };
 
