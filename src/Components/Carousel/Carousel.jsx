1b6a72dd2ad9ba383da47b09a22241bedd234fd0
Joel Mendez <joel.j.mendez.m@gmail.com>
2023-02-17 16:58:03 -0300

    Create Carousel component and edit it

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -4,6 +4,15 @@ import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
 import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 // And react-slick as our Carousel Lib
 import Slider from 'react-slick';
+import germany from '../../assets/hobbies/Germany.jpg';
+import argentina from '../../assets/hobbies/Argentina.png';
+import brazil from '../../assets/hobbies/Brazil.png';
+import chile from '../../assets/hobbies/Chile.jpg';
+import ecuador from '../../assets/hobbies/Ecuador.png';
+import france from '../../assets/hobbies/France.png';
+import netherlands from '../../assets/hobbies/Netherlands.png';
+import uruguay from '../../assets/hobbies/Uruguay.jpg';
+import venezuela from '../../assets/hobbies/Venezuela.png';
 
 // Settings for the slider
 const settings = {
@@ -30,9 +39,15 @@ export default function Carousel() {
 
 	// These are the images used in the slide
 	const cards = [
-		'https://images.unsplash.com/photo-1612852098516-55d01c75769a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-		'https://images.unsplash.com/photo-1627875764093-315831ac12f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-		'https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+		germany,
+		argentina,
+		brazil,
+		chile,
+		ecuador,
+		france,
+		netherlands,
+		uruguay,
+		venezuela,
 	];
 
 	return (
@@ -57,7 +72,7 @@ export default function Carousel() {
 			{/* Left Icon */}
 			<IconButton
 				aria-label='left-arrow'
-				colorScheme='messenger'
+				colorScheme='transparent'
 				borderRadius='full'
 				position='absolute'
 				left={side}
@@ -71,7 +86,7 @@ export default function Carousel() {
 			{/* Right Icon */}
 			<IconButton
 				aria-label='right-arrow'
-				colorScheme='messenger'
+				colorScheme='transparent'
 				borderRadius='full'
 				position='absolute'
 				right={side}
@@ -87,7 +102,8 @@ export default function Carousel() {
 				{cards.map((url, index) => (
 					<Box
 						key={index}
-						height={'6xl'}
+						height='200'
+						width='200'
 						position='relative'
 						backgroundPosition='center'
 						backgroundRepeat='no-repeat'
